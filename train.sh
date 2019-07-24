193acccc6eb0955d506ffe82caabbdf1e50293b1
chihming <changecandy@gmail.com>
2019-07-24 17:51:30 +0800

    use warp

---
+++ train.sh
@@ -0,0 +1,7 @@
+# train embeddings
+./smore/cli/warp -train ./exp/train.data -save exp/rep.txt -dimensions 128 -sample_times 500 -threads 8
+
+# generate recommendations
+python code/retrieve.py -train ./exp/train.data -test ./exp/dev.data -rep ./exp/rep.txt -worker 8 -topk 20
+python code/retrieve.py -train ./exp/train.data -test ./exp/test.data -rep ./exp/rep.txt -worker 8 -topk 20
+
